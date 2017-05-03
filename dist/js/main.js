ba6d63c77771b81f84f1813e403258bdaea80561
jsalisbury <joel.salisbury@gmail.com>
2017-05-03 12:45:45 -0400

    built

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,3 @@
 ;(function(window, $) {
-	
-	var navStart = 1;
-	$(".nav-toggle").on('click', function(){
-		navStart *= -1;
-		$(this).next("nav").toggle();
-		$(this).parents(".nav-holder").toggleClass('orange');
-
-			if(navStart == -1)
-				var src = "dist/img/BuiltBetter-Type.png"
-			else
-				var src = "dist/img/bb-logo.svg";
-		
-		$("#logo").attr('src',src);
-		$(this).toggleClass('reverse');
-
-	});
-
-
-
-
+	console.log("Hello!");
 })(window, jQuery);
