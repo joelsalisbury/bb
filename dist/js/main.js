7288576085b2a37be7ade992ae8fbfe10d0767a5
jsalisbury <joel.salisbury@gmail.com>
2017-05-03 14:39:00 -0400

    changes to footer, columns, cards, etc

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,22 @@
 ;(function(window, $) {
-	console.log("Hello!");
+	
+	var navStart = 1;
+	$(".nav-toggle").on('click', function(){
+		navStart *= -1;
+		$(this).next("nav").toggle();
+		$(this).parents(".nav-holder").toggleClass('orange');
+
+			if(navStart == -1)
+				var src = "dist/img/BuiltBetter-Type.png"
+			else
+				var src = "dist/img/bb-logo.svg";
+		
+		$("#logo").attr('src',src);
+		$(this).toggleClass('reverse');
+
+	});
+
+
+
+
 })(window, jQuery);
